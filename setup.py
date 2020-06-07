b7ce9666207bb2eaa2e762ad0f4f99c886c5708e
avasid <avasid@bt.iitr.ac.in>
2020-06-07 22:00:45 +0530

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+import setuptools
+
+def main():
+    
+    setuptools.setup(
+        name = "downloadipy",
+        version = "0.1.0",
+        description = "Internet downloader",
+        long_description = "Internet downloader with resume support",
+        url = "https://github.com/avasid/downloadiPy",
+        author = "Aakif Vasid",
+        author_email = "vasidaakif@gmail.com",
+        license = "MIT",
+        py_modules = ["downloadipy"],
+        entry_points = """
+            [console_scripts]
+            downloadipy = downloadipy:downloadipy
+        """
+    )
+if __name__ == "__main__":
+    main()
