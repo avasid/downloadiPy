6cea835ab318eee648945b20940cc039f6ec40aa
avasid <avasid@bt.iitr.ac.in>
2020-07-06 00:02:13 +0530

    Import packages in __init__ file

---
+++ __init__.py
@@ -0,0 +1,9 @@
+import cgi
+import gzip
+import os
+import sys
+import time
+import zlib
+
+import brotli
+import requests
--- downloadipy.py
+++ downloadipy.py
@@ -1,14 +1,3 @@
-import cgi
-import gzip
-import os
-import sys
-import time
-import zlib
-
-import brotli
-import requests
-
-
 class Downloader:
 
     def __init__(self, url: str, path: str = None, method: str = "GET", cookies: dict = None,
